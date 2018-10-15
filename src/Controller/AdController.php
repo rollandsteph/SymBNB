a048d6e9b7bfea796430a2b03cbef532c69121f8
rollandsteph <32583549+rollandsteph@users.noreply.github.com>
2018-10-16 01:39:00 +0200

    fin vidéo 42

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,18 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Ad;
+use App\Repository\AdRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class AdController extends AbstractController
 {
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index()
+    public function index(AdRepository $repo)
     {
+        $ads=$repo->findAll();
         return $this->render('ad/index.html.twig', [
-            'controller_name' => 'AdController',
+            'ads' => $ads
+        ]);
+    }
+
+    /**
+     * permet d'afficher une annonce
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     * @return void
+     */
+    public function show($slug, AdRepository $repo)
+    {
+        $ad=$repo->findOneBySlug($slug);
+        return $this->render('ad/show.html.twig',[
+            'ad'=>$ad
         ]);
     }
 }
